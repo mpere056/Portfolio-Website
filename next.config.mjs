1a6f827f1e584ce79a19156ae6bbc71b388e164d
nipun <marknperera@hotmail.com>
2025-08-08 21:41:36 -0400

    The RAG works!

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ensure we are not using legacy export pipeline
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
